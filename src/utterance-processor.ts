25b82105be17896980ff91e87db201c00c6789e9
nova <nova.io@pm.me>
2022-12-09 16:05:46 -0500

    add utterance processor, just void and logging for now

---
+++ src/utterance-processor.ts
@@ -0,0 +1,27 @@
+import { UtteranceHandler } from "./speech";
+
+const loggingUtteranceHandler: UtteranceHandler = (speech: string) => {
+    console.log(speech);
+    return null;
+};
+
+const voidUtteranceHandler: UtteranceHandler = () => { return null };
+
+export interface UtteranceProcessor {
+    onCompleteUtterance: UtteranceHandler;
+    onPartialUtterance: UtteranceHandler;
+}
+
+export class VoidUtteranceProcessor implements UtteranceProcessor {
+    onCompleteUtterance: UtteranceHandler = voidUtteranceHandler;
+    onPartialUtterance: UtteranceHandler = voidUtteranceHandler;
+}
+
+export class LoggingUtteranceProcessor implements UtteranceProcessor {
+    onCompleteUtterance: UtteranceHandler = loggingUtteranceHandler;
+    onPartialUtterance: UtteranceHandler = loggingUtteranceHandler;
+}
+
+export class AnsweringMachineUtternaceProcessor extends LoggingUtteranceProcessor {
+
+};
