d1653cff896bed8fe797a74d9b1bb9452aaaca74
nova <nova.io@pm.me>
2022-12-12 17:46:34 -0500

    tie new providers into call handler, wrap websocket behavior in provider function

--- src/call.ts
+++ src/call.ts
@@ -1,8 +1,9 @@
-import { WebSocket, WebSocketBehavior } from "uWebSockets.js";
+import { HttpRequest, HttpResponse, us_socket_context_t, WebSocket, WebSocketBehavior } from "uWebSockets.js";
 import { Connection, ConnectionManager, ConnectionUnavailableException } from "./connections";
 import { SystemPhoneNumber } from "./rolodex";
 import { RequestBehavior } from "./router";
-import { AzureSpeechToText, SpeechToText } from "./speech";
+import { AzureSpeechToText, SpeechToText, SpeechToTextProvider } from "./speech";
+import { UtteranceProcessor } from "./utterance-processor";
 
 const connectionManager = new ConnectionManager();
 
@@ -12,58 +13,59 @@ function getConnection(ws: WebSocket): Connection<WebSocket> {
   throw new ConnectionUnavailableException(ws['id']);
 }
 
-export const receive: WebSocketBehavior = {
-  /* There are many common helper features */
-  idleTimeout: 32,
-  maxBackpressure: 1024,
-  maxPayloadLength: 512,
-  // compression: DEDICATED_COMPRESSOR_3KB,
+function makeWebSocketReceiver(idHeader: string, processor: UtteranceProcessor, sttProvider: SpeechToTextProvider): WebSocketBehavior {
+  const idKey = 'id';
+  const receive: WebSocketBehavior = {
+    idleTimeout: 32,
+    maxBackpressure: 1024,
+    maxPayloadLength: 512,
+    // compression: DEDICATED_COMPRESSOR_3KB,
 
-  upgrade: (res, req, context) => {
-    console.log('An Http connection wants to become WebSocket, URL: ' + req.getUrl() + '!');
-    const phoneNumber = req.getHeader('trq-number-system');
+    upgrade: (res, req, context) => {
+      console.log('An Http connection wants to become WebSocket, URL: ' + req.getUrl() + '!');
+      const id: string = req.getHeader(idHeader);
+      if (id == "") {
+        res.end("400", true);
+        return;
+      }
 
+      res.upgrade(
+        { url: req.getUrl(), idKey: id },
+        req.getHeader('sec-websocket-key'),
+        req.getHeader('sec-websocket-protocol'),
+        req.getHeader('sec-websocket-extensions'),
+        context
+      );
+    },
 
-    /* This immediately calls open handler, you must not use res after this call */
-    res.upgrade(
-      { 
-        url: req.getUrl(),
-        id: phoneNumber
-      },
-      /* Spell these correctly */
-      req.getHeader('sec-websocket-key'),
-      req.getHeader('sec-websocket-protocol'),
-      req.getHeader('sec-websocket-extensions'),
-      context
-    );
-  },
-
-  open: (ws) => {
-    const id: SystemPhoneNumber = ws['id'];
-    console.log(`Websocket connection opened for ${id}`);
-
-    const conn: Connection<WebSocket> = new Connection(id, ws, AzureSpeechToText.create());
-    ws['conn'] = conn;
-    conn.transformer.listen();
-  },
-  message: (ws, message, isBinary) => {
-    /* Ok is false if backpressure was built up, wait for drain */
-    if (isBinary) {
-      // let ok = ws.send(message, isBinary);
+    open: (ws) => {
+      const id: SystemPhoneNumber = ws[idKey];
+      console.log(`Websocket connection opened for ${id}`);
+      const conn: Connection<WebSocket> = new Connection(id, ws, sttProvider.create(processor));
+      ws['conn'] = conn; //TODO: make more better
+      conn.transformer.listen();
+    },
+    message: (ws, message, isBinary) => {
+      /* Ok is false if backpressure was built up, wait for drain */
+      if (isBinary) {
+        // let ok = ws.send(message, isBinary);
+        const conn: Connection<WebSocket> = getConnection(ws);
+        conn.transformer.write(message);
+      } else {
+        console.error("Invalid Message: %s", message);
+      }
+    },
+    drain: (ws) => {
+      console.log('WebSocket backpressure: ' + ws.getBufferedAmount());
+    },
+    close: (ws, code, message) => {
       const conn: Connection<WebSocket> = getConnection(ws);
-      conn.transformer.write(message);
-    } else {
-      console.error("Invalid Message: %s", message);
+      conn.transformer.close();
+      console.log('WebSocket closed');
     }
-  },
-  drain: (ws) => {
-    console.log('WebSocket backpressure: ' + ws.getBufferedAmount());
-  },
-  close: (ws, code, message) => {
-    const conn: Connection<WebSocket> = getConnection(ws);
-    conn.transformer.close();
-    console.log('WebSocket closed');
   }
+
+  return receive;
 }
 
 export const event: RequestBehavior = (res, req) => {
